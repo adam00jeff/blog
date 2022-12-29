efdf173fe7ffe572fd1bd2b743f4c887a2ec2988
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-12-29 11:21:38 +0000

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <?= $post; ?>
+</article>
+
+</article>
+
+<a href="/">Go back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque autem commodi cumque debitis, dolore doloremque
+        enim et, ipsa magnam obcaecati officia perferendis quidem reiciendis, repellendus sapiente unde velit? Deleniti
+        doloribus dolorum facilis, impedit non quis tempora. Commodi iure, quos! Commodi cumque ea eaque fugit iusto
+        nemo odio quia voluptas? Autem.
+
+    </p>
+</article>
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias delectus deserunt dolor dolorem doloribus
+        explicabo, facere hic illum minus, nemo perferendis perspiciatis qui, temporibus? Accusamus consequuntur
+        delectus dolorum magnam maxime.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad amet beatae cumque, debitis eaque facilis fuga
+        impedit incidunt ipsum iusto magni nesciunt nisi omnis quae quam quasi, sint voluptatibus.
+
+    </p>
+</article>
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda, consequatur corporis debitis delectus
+        eaque error incidunt ipsum labore libero magnam maiores, maxime nam neque officia optio porro quaerat, quas
+        repellendus voluptatibus. Ab dolores illo quisquam repellat voluptate. Adipisci alias asperiores debitis illo
+        laboriosam laborum magni nobis omnis quos rem.
+
+    </p>
+</article>
+</body>
