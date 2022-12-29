79932ff323ed51211b8f0d2d3c3c28c56ff22b53
Adam Jeffreson <adam.jeffreson@gmail.com>
2022-12-29 12:36:58 +0000

    updated to lesson 13

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,23 +8,38 @@ use Illuminate\Support\Facades\File;
 
 class Post
 {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug=$slug;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
+     return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => \Spatie\YamlFrontMatter\YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
 
-        return array_map(fn($file) => $file->getContents(), $files);
     }
 
     public static function find($slug)
     {
-        base_path();
-        //uses resource path laravel helper function to find path to resource folder
-
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
+        return static::all()->firstWhere('slug', $slug);
     }
 
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,9 +3,16 @@
 <title>My Blog</title>
 <link rel="stylesheet" href="/app.css">
 <body>
-    <?php foreach ($posts as $post) : ?>
-    <article>
-        <?= $post; ?>
-    </article>
-    <?php endforeach; ?>
+<?php foreach ($posts as $post): ?>
+<article>
+    <h1>
+            <a href="/posts/<?= $post->slug; ?>">
+            <?= $post->title; ?><a/>
+    </h1>
+    <div>
+            <?= $post->excerpt; ?>
+    </div>
+</article>
+<?php endforeach; ?>
+<a href="/">Go back</a>
 </body>
